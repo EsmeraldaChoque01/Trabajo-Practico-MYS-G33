36afb7cc4f04f1d54d07c03bece4465e3741881c
guadasilva <guadalupe.silva@mi.unc.edu.ar>
2026-06-14 01:27:07 -0300

    Update Servidores.py

--- Servidores.py
+++ Servidores.py
@@ -2,7 +2,8 @@ import random
 import numpy as np
 import math
 from collections import deque
-
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 # --------------------------------------------- EJERCICIO 1)A)  -------------------------------------------------------------------------
 
 #generacion de eventos usando poisson con adelgazamiento:
@@ -169,4 +170,115 @@ media_p2 = np.mean(prop_s2)
 
 print("Proporción promedio S1:", media_p1)
 print("Proporción promedio S2:", media_p2)
+
+# --------------------------------------------------- EJERCICIO 1) C ) --------------------------------------------------------------
+
+#Grafico del histograma
+
+tiempo_permanencia = []
+simulaciones = simular_sistema_servidores(10000)
+for  ts, ta in zip(simulaciones[1], simulaciones[2]):
+  tiempo_permanencia.append(ts- ta)
+
+fig, ax = plt.subplots()
+ax.hist(tiempo_permanencia, bins=50, color='m', edgecolor='black', alpha=0.7)
+plt.title("Histograma de tiempos de permanencia")
+plt.xlabel("Tiempo de permanencia")
+plt.ylabel("Frecuencia", rotation=0, labelpad=30)
+plt.show()
+
+#Propuesta de distribuciones teoricas:
+frecuencias_obs, extremos = np.histogram(tiempo_permanencia, bins=5)
+
+#GAMMA:
+#Estimacion de parametros alfa y beta a partir de la muestra de datos
+m_est = np.mean(tiempo_permanencia)
+var_est = np.var(tiempo_permanencia, ddof=1)
+alfa_est = m_est**2 / var_est
+beta_est = var_est / m_est
+
+#Calculo de las probabilidades teoricas
+probs_est = []
+for i in range(len(frecuencias_obs)):
+  prob_i_est = stats.gamma.cdf(extremos[i+1], a=alfa_est, scale=beta_est) - stats.gamma.cdf(extremos[i], a=alfa_est, scale=beta_est)
+  probs_est.append(prob_i_est)
+
+#Conteo de las frecuecias observadas en la muestra de datos
+Ni_obs = [val for val in frecuencias_obs]
+
+#Construccion del estadistico T
+n = len(tiempo_permanencia)
+T = 0
+for i in range(len(Ni_obs)):
+  T += ((Ni_obs[i] - n*probs_est[i])**2) / (n*probs_est[i])
+    
+#Calculo del p-valor usando simulaciones
+p_valor = 0
+for sim in range(1000):
+  muestra_sim = np.random.gamma(alfa_est, beta_est, size=10000)
+  m = len(muestra_sim)
+  frecuencias_sim, extremos_sim= np.histogram(muestra_sim, bins=5)
+
+  m_sim = np.mean(muestra_sim)
+  var_sim = np.var(muestra_sim, ddof=1)
+
+  alfa_sim = m_sim**2 / var_sim
+  beta_sim = var_sim / m_sim
+
+  probs_sim = []
+  for i in range(len(frecuencias_sim)):
+    prob_i_sim = stats.gamma.cdf(extremos_sim[i+1], a=alfa_sim, scale=beta_sim) - stats.gamma.cdf(extremos_sim[i], a=alfa_sim, scale=beta_sim)
+    probs_sim.append(prob_i_sim)
+  
+  Ni_sim = [val for val in frecuencias_sim]
+
+  T_sim = 0
+  for i in range(len(Ni_sim)):
+    T_sim += ((Ni_sim[i] - m*probs_sim[i])**2) / (m*probs_sim[i])
+  if T_sim > T:
+    p_valor += 1
+print(f"El p-valor es: {p_valor/1000}")
+
+#EXPONENCIAL
+#Estimacion del parametro lambda
+m_est = np.mean(tiempo_permanencia)
+lambda_est = 1/m_est
+
+#Calculo de probabilidades
+probs_est = []
+for i in range(len(frecuencias_obs)):
+  prob_i_est = stats.expon.cdf(extremos[i+1], scale=1/lambda_est) - stats.expon.cdf(extremos[i], scale=1/lambda_est)
+  probs_est.append(prob_i_est)
+
+#Frecuencias observadas
+Ni_obs = [val for val in frecuencias_obs]
+
+#Estadistico T
+T = 0
+for i in range(len(Ni_obs)):
+  T += ((Ni_obs[i] - len(tiempo_permanencia)*probs_est[i])**2) / (len(tiempo_permanencia)*probs_est[i])
+print(T)
+
+#Calculo del p-valor con simulaciones
+p_valor = 0
+for sim in range(10000):
+  muestra_sim = np.random.exponential(scale=1/lambda_est, size=10000)
+  lamda_sim = 1/np.mean(muestra_sim)
+  m = len(muestra_sim)
+  frecuencias_sim, extremos_sim= np.histogram(muestra_sim, bins=5)
+  probs_sim = []
+  for i in range(len(frecuencias_sim)):
+    prob_i_sim = stats.expon.cdf(extremos_sim[i+1], scale=1/lamda_sim) - stats.expon.cdf(extremos_sim[i], scale=1/lamda_sim)
+    probs_sim.append(prob_i_sim)
+  
+  Ni_sim = [val for val in frecuencias_sim]
+
+  T_sim = 0
+  for i in range(len(Ni_sim)):
+    T_sim += ((Ni_sim[i]-m*probs_sim[i])**2) / (m*probs_sim[i])
+  
+  if T_sim > T:
+    p_valor += 1
+
+print(f"El p-valor es: {p_valor/10000}")
 
